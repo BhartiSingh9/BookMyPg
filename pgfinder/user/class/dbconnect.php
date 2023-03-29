4ec6e5f19b2f686847cdd1797f61d7b7b6366adf
Bharti Singh <bharti.singhbs9681@gmail.com>
2023-03-29 23:21:50 +0530

    Initial commit

---
+++ pgfinder/user/class/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+    $host="localhost:3307";
+    $user="root";
+    $passwd="";
+    $dbname="bookmypg";
+    
+    $connection= mysqli_connect($host, $user, $passwd, $dbname);
+            
+?>
